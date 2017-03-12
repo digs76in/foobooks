5c79dbc9155981eec980f3627894d9a34d177aba
Digant Shah <digs76in@gmail.com>
2017-03-12 16:54:41 -0400

    Practice controller setup, environments, packages

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,34 @@ Route::get('/books', 'BookController@index');
 Route::get('/books/{title?}', 'BookController@view');
 # New route get welcome file using WelcomeController invoke method
 Route::get('/', 'WelcomeController');
+# Temp Practice route
+/*Route::get('/practice', function() {
+    echo config('mail.driver');
+});*/
+
+/**
+* Practice controller route
+*/
+Route::any('/practice/{n?}', 'PracticeController@index');
+/**
+* Debugbar test route
+*/
+Route::get('/debugbar', function() {
+
+    $data = Array('foo' => 'bar');
+    Debugbar::info($data);
+    Debugbar::info('Current environment: '.App::environment());
+    Debugbar::error('Error!');
+    Debugbar::warning('Watch out…');
+    Debugbar::addMessage('Another message', 'mylabel');
+
+    return 'Just demoing some of the features of Debugbar';
+
+});
+/**
+* LaravelLogViewer test route
+*/
+
+if(config('app.env')=='local'){
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
